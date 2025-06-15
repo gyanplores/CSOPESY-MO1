e8e8a924f68a25d150068a4d114a389fcb971f14
Giancarlo A. Flores <gyanplores@gmail.com>
2025-06-15 12:11:21 +0800

    singleton start

--- ConsoleManager.cpp
+++ ConsoleManager.cpp
@@ -1,9 +1,13 @@
-#pragma once
-
 #include "ConsoleManager.h"
 #include "MainConsole.h"
 #include "SchedulingConsole.h"
 
 #include <iostream>
 
-const String 
+const String MAIN_CONSOLE = "MAIN_CONSOLE";
+const String SCHEDULING_CONSOLE = "SCHEDULING_CONSOLE";
+
+class ConsoleManager{
+    public:
+    
+}
--- ConsoleManager.h
+++ ConsoleManager.h
@@ -1,6 +1,25 @@
 #pragma once
+#include "Console.h"
+
+const String MAIN = "MAIN_CONSOLE"
+const String MARQUEE = "MARQUEE_CONSOLE"
+const String SHEDULE = "SCHEDULE_CONSOLE"
+
+
 
 class ConsoleManager{
 public:
-    
+    typedef std::unordered_map<String, std::shared_ptr<Console>> ConsoleTable;
+
+    static ConsoleManager* getInstance();
+    static void initialize_console_manager();
+    static void destroy_console_manager();
+private: 
+    ConsoleManager();
+    ~ConsoleManager() = default
+    ConsoleManager(ConsoleManager const&) {}
+    ConsoleManager& operator=(ConsoleManager const&) {};
+    static ConsoleManager* sharedInstance;
+
+    ConsoleTable consoleTable
 };
