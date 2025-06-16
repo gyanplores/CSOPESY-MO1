f0f9520ed625cf27824bb07e60162adf99f1e5d4
Agi-Jer <127390932+Agi-Jer@users.noreply.github.com>
2025-06-16 20:22:53 +0800

    Process.h comments

--- Process.h
+++ Process.h
@@ -16,19 +16,20 @@ class Process {
     public:
         int id; //ID of process
         int instruction_lines_max; //all lines of instructions of process - defaults to print currently
-        int instruction_lines_current = 0;
+        int instruction_lines_current = 0; // currentline of the process, updates automatically
 
         int current_core;
 
-        time_t timestamp;
+        time_t timestamp; //Is set when its declared and updates when is being processed by core
 
         ProcessState state = READY;
 
     public:
         Process(int i, int n);
         std::string setCurrentTime();
-        static std::vector<Process> print_processes();
+        static std::vector<Process> print_processes(); //static method that return the 10 process we need for the homework
         
+        //setter function for the state variable
         void setRunning();
         void setFinished();
 };
