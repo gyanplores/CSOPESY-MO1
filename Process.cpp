dbdd02f43387b49e469a24ba2abce4ad7b5d6a4f
Agi-Jer <127390932+Agi-Jer@users.noreply.github.com>
2025-06-28 22:08:27 +0800

    randomize print number

--- Process.cpp
+++ Process.cpp
@@ -1,7 +1,9 @@
 #include "Process.h"
+#include "ConsoleManager.h"
 #include <iostream>
 #include <vector>
 #include <string>
+
 
 Process::Process(int i, int n) : id(i), instruction_lines_max(n) { time(&timestamp); }
 
@@ -11,7 +13,7 @@ std::vector<Process> Process::print_processes() {
     static std::vector<Process> v;
 
     for (int i = 0; i < 10; i++){
-        v.push_back(Process(i, 100));
+        v.push_back(Process(i, min_ins + (rand() % (max_ins - min_ins + 1))));
     }
 
     return v;
